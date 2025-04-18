220b5eb5d7e103878c20e3bcd1f4110153f56c25
kurir123 <usfahstore@gmail.com>
2025-04-18 20:50:42 +0700

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,10 +1,9 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function App() {
+  return <h1>Kurir App Siap Deploy!</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
